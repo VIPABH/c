06f06b1b4189884349526deb6ddccbd9de6245a0
VIPABH <a891983.2@gmail.com>
2025-11-07 19:06:20 +0300

    Update code.py

--- code.py
+++ code.py
@@ -2,9 +2,12 @@ from telethon import events, Button
 from Resources import *
 from ABH import *
 devrs = [1910015590, 1418998675]
+print('i am live')
 b = [Button.inline('اضافة مراقب', data="addwatcher"), Button.inline('اضافة ناشر', data='addpostman')]
 @ABH.on(events.NewMessage(pattern='/start'))
 async def start(e):
     id = e.sender_id
-    if id in devrs:
-        await e.reply(f'اهلا عزيزي ( {await mention(e)} ) شنو تحب تسوي اليوم؟', button=b)
+    if not id in devrs:
+        await e.reply('🙂')
+        return
+    await e.reply(f'اهلا عزيزي ( {await mention(e)} ) شنو تحب تسوي اليوم؟', button=b)
