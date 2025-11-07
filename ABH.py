b15b2c73617577969835ba676b0a25b57963ec7d
VIPABH <a891983.2@gmail.com>
2025-11-07 18:47:24 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -1,6 +1,12 @@
 from telethon import events ,TelegramClient
+from code import *
 import os
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BOT_TOKEN")
 ABH = TelegramClient("code", api_id, api_hash).start(bot_token=bot_token)
+def main():
+    ABH.start(bot_token=bot_token)
+    ABH.run_until_disconnected()
+if __name__ == "__main__":
+    main()
