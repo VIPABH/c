2d52b102fafaeeb7827b3d89ca63d531ac6e2ada
VIPABH <a891983.2@gmail.com>
2025-11-07 19:03:46 +0300

    Update ABH.py

--- ABH.py
+++ ABH.py
@@ -1,12 +1,13 @@
-from telethon import events ,TelegramClient
-from code import *
+from telethon import TelegramClient, events
 import os
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BOT_TOKEN")
-ABH = TelegramClient("code", api_id, api_hash).start(bot_token=bot_token)
-def main():
-    ABH.start(bot_token=bot_token)
-    ABH.run_until_disconnected()
+ABH = TelegramClient("ABH_session", api_id, api_hash)
+async def main():
+    await ABH.start(bot_token=bot_token)
+    print("✅ Bot is running and connected successfully.")
+    await ABH.run_until_disconnected()
 if __name__ == "__main__":
-    main()
+    import asyncio
+    asyncio.run(main())
